75b2854c8972bf948d55ceee679bea93e55002af
Ammarahmad786 <91366905+Ammarahmad786@users.noreply.github.com>
2024-08-07 14:37:05 +0500

    Rebate_Calendar

---
+++ Smartwyre.DeveloperTest.Runner/Program.cs
@@ -0,0 +1,42 @@
+﻿using Smartwyre.DeveloperTest.Interfaces;
+using Smartwyre.DeveloperTest.Services;
+using Smartwyre.DeveloperTest.Calculators;
+using Smartwyre.DeveloperTest.Data;
+using Smartwyre.DeveloperTest.Types;
+using System;
+using System.Collections.Generic;
+
+namespace Smartwyre.DeveloperTest.Runner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IRebateDataStore rebateDataStore = new RebateDataStore();
+            IProductDataStore productDataStore = new ProductDataStore();
+
+            var calculators = new Dictionary<IncentiveType, IRebateCalculator>
+            {
+                { IncentiveType.FixedCashAmount, new FixedCashAmountCalculator() },
+                { IncentiveType.FixedRateRebate, new FixedRateRebateCalculator() },
+                { IncentiveType.AmountPerUom, new AmountPerUomCalculator() }
+            };
+
+            IRebateService rebateService = new RebateService(rebateDataStore, productDataStore, calculators);
+
+            // Example request
+            var request = new CalculateRebateRequest
+            {
+                RebateIdentifier = "1",
+                ProductIdentifier = "A",
+                Volume = 10
+            };
+
+            // Perform calculation
+            var result = rebateService.Calculate(request);
+
+            // Output the result
+            Console.WriteLine($"Success: {result.Success}, Rebate Amount: {result.RebateAmount}");
+        }
+    }
+}
